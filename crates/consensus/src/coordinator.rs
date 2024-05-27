e8918bb8a24cb4a6373d522a553d503fc05c3dab
lfbrehm <97600985+lfbrehm@users.noreply.github.com>
2024-05-27 17:39:56 +0200

    feat: broadcast and quorum

--- crates/consensus/src/coordinator.rs
+++ crates/consensus/src/coordinator.rs
@@ -2,10 +2,11 @@ use std::sync::Arc;
 use anyhow::{anyhow, Result};
 use bytes::Bytes;
 use tonic::transport::Channel;
-use consensus_transport::consensus_transport::{CommitRequest, Dependency, PreAcceptRequest, State};
+use consensus_transport::consensus_transport::{AcceptRequest, AcceptResponse, ApplyRequest, ApplyResponse, CommitRequest, CommitResponse, Dependency, PreAcceptRequest, PreAcceptResponse, State};
 use consensus_transport::consensus_transport::consensus_transport_client::ConsensusTransportClient;
 use diesel_ulid::DieselUlid;
 use tokio::task::JoinSet;
+use tonic::Response;
 
 #[derive(Clone, Debug)]
 pub struct Coordinator {
@@ -39,16 +40,30 @@ impl Transaction {
             t: t_zero.clone(),
             dependencies: Vec::new(),
         }
-        
+
     }
-    pub fn pre_accept(&mut self, t: Bytes) -> Result<()> {
+    pub fn pre_accept(&mut self, t: Vec<u8>) -> Result<()> {
+        let new_ulid = DieselUlid::try_from(t.as_slice())?;
+        if self.t.timestamp() < new_ulid.timestamp() {
+            self.t = new_ulid;
+        }
         Ok(())
     }
 }
 
-pub enum Path {
-    Fast,
-    Slow,
+enum ConsensusRequest {
+    PreAccept(PreAcceptRequest),
+    Accept(AcceptRequest),
+    Commit(CommitRequest),
+    Apply(ApplyRequest),
+    // TODO: Recover
+}
+
+enum ConsensusResponse {
+    PreAccept(PreAcceptResponse),
+    Accept(AcceptResponse),
+    Commit(CommitResponse),
+    Apply(ApplyResponse),
 }
 
 impl Coordinator {
@@ -56,73 +71,158 @@ impl Coordinator {
         todo!("Add members to self")
     }
     pub async fn transaction(&self, transaction: Bytes) -> Result<()> {
-        
+        /*
+            INIT
+        */
+
         // We need a fixed size of members for each transaction
         let members = self.members.clone();
-       
+
         // Create a new transaction state
         let mut transaction = Transaction::init(transaction);
+
+        /*
+            PRE ACCEPT
+         */
         
         // Create the PreAccept msg
         let pre_accept_request = PreAcceptRequest {
             node: self.node.clone(),
             timestamp_zero: transaction.t_zero.as_byte_array().into(),
-            event: transaction.transaction.into(),
+            event: transaction.transaction.clone().into(),
         };
-        
+
         let mut pre_accept_responses =  JoinSet::new() ;
-        
+
         // Send PreAccept request to every known member
         for replica in &members {
-            let mut client = ConsensusTransportClient::new(replica.channel.clone());
-            let response = client.pre_accept(pre_accept_request.clone());
-            pre_accept_responses.spawn(response);
+            let channel = replica.channel.clone();
+            let request = pre_accept_request.clone();
+            pre_accept_responses.spawn(async move {
+                let mut client = ConsensusTransportClient::new(channel);
+                client.pre_accept(request).await
+            });
         }
-        
+
         // await JoinSet
-       
-        // Implement any/contains/find manually, because you cannot return Results ...
-        let mut path = Path::Fast;
-        for resp in pre_accept_responses.iter() {
-            let time_slice = <&[u8; 16]>::try_from(resp.timestamp.as_slice())?;
-            if DieselUlid::from(time_slice) != transaction.t_zero {
-                continue
-            } else {
-                path = Path::Slow;
+        let majority = (members.len()/2) + 1;
+        let mut counter = 0_usize;
+
+        // Poll majority
+        while let Some(response) = pre_accept_responses.join_next().await {
+            let response = response??.into_inner();
+            transaction.pre_accept(response.timestamp)?;
+
+            counter += 1;
+            if counter >= majority {
                 break;
             }
         }
-
-        // TODO: 
-        match path {
-            Path::Fast => {
-                let commit_request = CommitRequest {
-                    node: self.node.clone(),
-                    timestamp_zero: transaction.t_zero.as_byte_array().into(),
-                    timestamp: transaction.t.as_byte_array().into(),
-                    dependencies: vec![],
-                };
-                for replica in &members {
-                    let mut client = ConsensusTransportClient::new(replica.channel.clone());
-                    let response = client.commit(commit_request.clone()).await?.into_inner();
-                }
-                // - fast path
-            }
-            Path::Slow => {
-                // - slow path
-            }
-        }            
+        
+        
+        if transaction.t_zero == transaction.t {
+            /*
+                FAST PATH 
+            */
+            for 
+            
+        } else {
+            /*
+                SLOW PATH
+            */
+        }
         // - execution
         // - recovery
-        
-        
+
+
         Err(anyhow!("Not yet implemented"))
     }
-    
+
     async fn execute() -> Result<()> {
         todo!("Impl execute")
     }
     async fn apply() -> Result<()> {
        todo!("Apply all persistent changes when rebooting")
     }
+    
+    async fn broadcast(members: &[Member], request: ConsensusRequest) -> Result<Vec<ConsensusResponse>> {
+        let mut responses =  JoinSet::new() ;
+        let mut result = Vec::new();
+
+        // Send PreAccept request to every known member
+        for replica in members {
+            let channel = replica.channel.clone();
+            match &request {
+                ConsensusRequest::PreAccept(req) => {
+                    responses.spawn(async move {
+                        let mut client = ConsensusTransportClient::new(channel);
+                        client.pre_accept(req.clone()).await.try_into()?
+                    });        
+                }
+                ConsensusRequest::Accept(req) => {
+                    responses.spawn(async move {
+                        let mut client = ConsensusTransportClient::new(channel);
+                        client.accept(req.clone()).await.try_into()?
+                    });
+                }
+                ConsensusRequest::Commit(req) => {
+                    responses.spawn(async move {
+                        let mut client = ConsensusTransportClient::new(channel);
+                        client.commit(req.clone()).await.try_into()?
+                    });
+                }
+                ConsensusRequest::Apply(req) => {
+                    responses.spawn(async move {
+                        let mut client = ConsensusTransportClient::new(channel);
+                        client.apply(req.clone()).await.try_into()?
+                    });
+                }
+            };
+            
+        }
+
+        // await JoinSet
+        let majority = (members.len()/2) + 1;
+        let mut counter = 0_usize;
+
+        // Poll majority
+        while let Some(response) = responses.join_next().await {
+            result.push(response?);
+            
+            counter += 1;
+            if counter >= majority {
+                break;
+            }
+        }
+        Ok(result)
+    }
+}
+
+impl TryFrom<Result<PreAcceptResponse>> for ConsensusResponse {
+    type Error = anyhow::Error;
+    fn try_from(value: Result<PreAcceptResponse>) -> Result<Self> {
+        Ok(ConsensusResponse::PreAccept(value?))
+    }
+    
+}
+impl TryFrom<Result<AcceptResponse>> for ConsensusResponse {
+    type Error = anyhow::Error;
+    fn try_from(value: Result<AcceptResponse>) -> Result<Self> {
+        Ok(ConsensusResponse::Accept(value?))
+    }
+
+}
+impl TryFrom<Result<CommitResponse>> for ConsensusResponse {
+    type Error = anyhow::Error;
+    fn try_from(value: Result<CommitResponse>) -> Result<Self> {
+        Ok(ConsensusResponse::Commit(value?))
+    }
+
+}
+impl TryFrom<Result<ApplyResponse>> for ConsensusResponse {
+    type Error = anyhow::Error;
+    fn try_from(value: Result<ApplyResponse>) -> Result<Self> {
+        Ok(ConsensusResponse::Apply(value?))
+    }
+
 }
--- crates/consensus_transport/src/consensus_transport.rs
+++ crates/consensus_transport/src/consensus_transport.rs
@@ -95,6 +95,28 @@ pub struct RecoverResponse {
     pub superseding: ::prost::alloc::vec::Vec<Dependency>,
 }
 #[derive(serde::Deserialize, serde::Serialize)]
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct AcceptRequest {
+    #[prost(string, tag = "1")]
+    pub node: ::prost::alloc::string::String,
+    #[prost(bytes = "vec", tag = "2")]
+    pub timestamp_zero: ::prost::alloc::vec::Vec<u8>,
+    #[prost(bytes = "vec", tag = "3")]
+    pub timestamp: ::prost::alloc::vec::Vec<u8>,
+    #[prost(message, repeated, tag = "4")]
+    pub dependencies: ::prost::alloc::vec::Vec<Dependency>,
+}
+#[derive(serde::Deserialize, serde::Serialize)]
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct AcceptResponse {
+    #[prost(string, tag = "1")]
+    pub node: ::prost::alloc::string::String,
+    #[prost(message, repeated, tag = "2")]
+    pub dependencies: ::prost::alloc::vec::Vec<Dependency>,
+}
+#[derive(serde::Deserialize, serde::Serialize)]
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
 #[repr(i32)]
 pub enum State {
@@ -272,6 +294,30 @@ pub mod consensus_transport_client {
                 );
             self.inner.unary(req, path, codec).await
         }
+        pub async fn accept(
+            &mut self,
+            request: impl tonic::IntoRequest<super::AcceptRequest>,
+        ) -> std::result::Result<tonic::Response<super::AcceptResponse>, tonic::Status> {
+            self.inner
+                .ready()
+                .await
+                .map_err(|e| {
+                    tonic::Status::new(
+                        tonic::Code::Unknown,
+                        format!("Service was not ready: {}", e.into()),
+                    )
+                })?;
+            let codec = tonic::codec::ProstCodec::default();
+            let path = http::uri::PathAndQuery::from_static(
+                "/consensus_transport.ConsensusTransport/Accept",
+            );
+            let mut req = request.into_request();
+            req.extensions_mut()
+                .insert(
+                    GrpcMethod::new("consensus_transport.ConsensusTransport", "Accept"),
+                );
+            self.inner.unary(req, path, codec).await
+        }
         pub async fn apply(
             &mut self,
             request: impl tonic::IntoRequest<super::ApplyRequest>,
@@ -343,6 +389,10 @@ pub mod consensus_transport_server {
             &self,
             request: tonic::Request<super::CommitRequest>,
         ) -> std::result::Result<tonic::Response<super::CommitResponse>, tonic::Status>;
+        async fn accept(
+            &self,
+            request: tonic::Request<super::AcceptRequest>,
+        ) -> std::result::Result<tonic::Response<super::AcceptResponse>, tonic::Status>;
         async fn apply(
             &self,
             request: tonic::Request<super::ApplyRequest>,
@@ -508,6 +558,52 @@ pub mod consensus_transport_server {
                     let fut = async move {
                         let inner = inner.0;
                         let method = CommitSvc(inner);
+                        let codec = tonic::codec::ProstCodec::default();
+                        let mut grpc = tonic::server::Grpc::new(codec)
+                            .apply_compression_config(
+                                accept_compression_encodings,
+                                send_compression_encodings,
+                            )
+                            .apply_max_message_size_config(
+                                max_decoding_message_size,
+                                max_encoding_message_size,
+                            );
+                        let res = grpc.unary(method, req).await;
+                        Ok(res)
+                    };
+                    Box::pin(fut)
+                }
+                "/consensus_transport.ConsensusTransport/Accept" => {
+                    #[allow(non_camel_case_types)]
+                    struct AcceptSvc<T: ConsensusTransport>(pub Arc<T>);
+                    impl<
+                        T: ConsensusTransport,
+                    > tonic::server::UnaryService<super::AcceptRequest>
+                    for AcceptSvc<T> {
+                        type Response = super::AcceptResponse;
+                        type Future = BoxFuture<
+                            tonic::Response<Self::Response>,
+                            tonic::Status,
+                        >;
+                        fn call(
+                            &mut self,
+                            request: tonic::Request<super::AcceptRequest>,
+                        ) -> Self::Future {
+                            let inner = Arc::clone(&self.0);
+                            let fut = async move {
+                                <T as ConsensusTransport>::accept(&inner, request).await
+                            };
+                            Box::pin(fut)
+                        }
+                    }
+                    let accept_compression_encodings = self.accept_compression_encodings;
+                    let send_compression_encodings = self.send_compression_encodings;
+                    let max_decoding_message_size = self.max_decoding_message_size;
+                    let max_encoding_message_size = self.max_encoding_message_size;
+                    let inner = self.inner.clone();
+                    let fut = async move {
+                        let inner = inner.0;
+                        let method = AcceptSvc(inner);
                         let codec = tonic::codec::ProstCodec::default();
                         let mut grpc = tonic::server::Grpc::new(codec)
                             .apply_compression_config(
