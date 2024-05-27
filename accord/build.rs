f30f521f2617a242475841a5a02e43012bb9da02
lfbrehm <97600985+lfbrehm@users.noreply.github.com>
2024-05-27 09:50:27 +0200

    feat: Generated prost-build in accord package

--- accord/build.rs
+++ accord/build.rs
@@ -1,4 +1,14 @@
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("src/protos/accord.proto")?;
-    Ok(())
+extern crate tonic_build;
+fn main() {
+    // tonic_build::compile_protos("src/protos/accord.proto")?;
+    tonic_build::configure()
+        .type_attribute(".", "#[derive(serde::Deserialize, serde::Serialize)]")
+        .compile_well_known_types(true)
+        .build_server(true)
+        .out_dir("./src/")
+        .compile(
+            &["./src/protos/accord.proto"],
+           &["./src/protos"],
+        )
+        .unwrap();
 }
